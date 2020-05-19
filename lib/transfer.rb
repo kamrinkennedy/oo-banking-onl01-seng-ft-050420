da75a6ad3d760212d1f7ace908be1df25e3977bb
Kamrin Kennedy <kamrinkennedy@gmail.com>
2020-05-19 17:48:22 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,10 @@ class Transfer
   end
   
   def execute_transaction 
-    
+    if (self.valid? && sender.balance >= amount)
+      sender.balance -= amount
+      receiver.balance += amount
+    end
   end
   
   def reverse_transfer
