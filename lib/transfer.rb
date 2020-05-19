cb2f23f20a072b5f56e6af716d0b9782f705d537
Kamrin Kennedy <kamrinkennedy@gmail.com>
2020-05-19 17:41:20 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,4 +1,12 @@
 class Transfer
-  attr_accessor :sender, :reciever, :status
-  attr_reader 
+  attr_accessor :sender, :reciever, :status, :amount
+  # attr_reader :amount
+  
+  def initialize(sender, reciever, amount)
+    @sender = sender
+    @reciever = reciever 
+    @amount = amount
+    @status = 'pending'
+  end
+  
 end
